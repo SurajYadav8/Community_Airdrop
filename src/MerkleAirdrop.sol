a7af24cf27b1efcbb4cd330345d36d55cdb40c9e
SurajYadav8 <raosahab1e@gmail.com>
2025-10-06 07:05:35 +0000

    commit

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
+pragma solidity ^0.8.24;
 
 contract MerkleAirdrop {
-    //some address
+    // some list of addresses
+    // Allow someone to claim tokens if they are in the list
 }
