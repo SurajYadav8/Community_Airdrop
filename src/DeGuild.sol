cccb37a0bda2ad5c8796a37059da7babdb90aa9f
SurajYadav8 <raosahab1e@gmail.com>
2025-10-06 11:53:37 +0000

    commit

--- src/DeGuild.sol
+++ src/DeGuild.sol
@@ -5,5 +5,5 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DeGuild is ERC20, Ownable {
-    constructor() ERC20("DeGuild", "DG") {}
+    constructor() ERC20("DeGuild", "DG") Ownable(msg.sender) {}
 }
--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -2,6 +2,18 @@
 pragma solidity ^0.8.24;
 
 contract MerkleAirdrop {
-// some list of addresses
-// Allow someone to claim tokens if they are in the list
+    // some list of addresses
+    // Allow someone to claim tokens if they are in the list
+    address[] eligibleAddresses;
+
+    // function claimAirdrop(address eligibleAccount) external {
+    //     for (uint256 i = 0; i < eligibleAddresses.length; i++) {
+    //         if (eligibleAddresses[i] == eligibleAccount) {}
+    //     }
+    // }
+
+    /**Note: using array to claim airdrop can be very costly and even sometimes fail due to gas limits.
+     * For example if we have 1000 addresses in the array, then we will have to loop through 1000 addresses.
+     * that's why we use Merkle Tree to optimize this process.
+     */
 }
