7c301527ffb59a761fec6da683979d99b040d978
SurajYadav8 <raosahab1e@gmail.com>
2025-10-07 16:35:03 +0000

    commit

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.24;
 
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
-
 contract MerkleAirdrop {
     using SafeERC20 for IERC20;
     // some list of addresses
@@ -34,7 +33,21 @@ contract MerkleAirdrop {
     }
 
     function claimAirdrop(address account, uint256 amount, bytes32[] calldata merkleProof) external {
-        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
+        // bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount)))); // works too but more expensive
+
+        bytes32 leaf; // declared outside so do not clash between YUL and solidity
+
+        // Using YUL assembly to optimize gas cost
+        assembly {
+            let ptr := mload(0x40)
+            mstore(ptr, amount)
+            mstore(add(ptr, 0x20), account)
+            let firstHash := keccak256(ptr, 0x40)
+
+            mstore(ptr, firstHash)
+            leaf := keccak256(ptr, 0x20)
+        }
+        
 
         if (!MerkleProof.verify(merkleProof, I_MERKLE_ROOT, leaf)) {
             revert MerkleAirdrop_InvalidProof();
